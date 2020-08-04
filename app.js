7c881c1291d6b408fc9d60b0ac404ba4d8010272
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-08-04 13:56:55 -0500

    first inquire prompt

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+//const fs = require('fs');
+
+//const generatePage = require('./src/page-template.js');
 
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
@@ -9,8 +11,17 @@ const [name, github] = profileDataArgs;
 //removed generatePage function to its own page-template.js file to reduce bloat on this file//
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-if (err) throw err;
+//fs.writeFile('index.html', generatePage(name, github), err => {
+//if (err) throw err;
 
-console.log('portfolio complerte! checkout index.html to see the output');
-});
+//console.log('portfolio complerte! checkout index.html to see the output');
+//});
+inquirer
+    .prompt([
+        {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
